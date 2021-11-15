9d51327810c4f60f7a0dcea8e56dbf80b82d357f
PeterFromSweden <peterfromswe884@gmail.com>
2021-11-15 10:37:25 +0100

    Evolved a bit on rename. Time before 12 was not ok, needed to replace leading space with 0.

--- filteraction.cmd
+++ filteraction.cmd
@@ -1,3 +1,16 @@
-set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+:: Script to be used from xgate mail
+:: Use with F8-key in inbox, one mail at a time
+
+:: cd to script folder
+cd %~dp0
+
+:: Strip any leading spaces
+Set _time=%time: =0%
+
+:: Create date-time string suitable for filename
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%_time:~0,2%_%_time:~3,2%_%_time:~6,2%
+move leaderboard.csv leaderboard%datetimef%.csv
 move expedition.txt expedition%datetimef%.txt
 move /y boatids.txt boat-ids.txt
+
+
